815b4f174e33b35bad742b93ba534dee985d4b58
Julie Saurat <julie.saurat.s@gmail.com>
2021-02-10 13:12:01 +0100

    add restaurant controler & index/show/new views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurant, except: [:update, :destroy] do
+  resources :restaurants, except: [:update, :destroy] do
     resources :review, only: [ :new, :create ]
   end
 
