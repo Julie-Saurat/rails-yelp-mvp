7f27433acec14a29a09ffb09c79d65dd8dc91cb6
Julie Saurat <julie.saurat.s@gmail.com>
2021-02-10 12:42:26 +0100

    tables, models & routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurant, except: [:update, :destroy] do
+    resources :review, only: [ :new, :create ]
+  end
+
+  resources :review, only: [:show]
+
 end
