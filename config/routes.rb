87dcea7a434838d2e50ede02eda992bd352e65cb
Julie Saurat <julie.saurat.s@gmail.com>
2021-02-10 15:15:06 +0100

    Yelp MVP - completed exercice

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants, except: [:update, :destroy] do
-    resources :reviews, only: [ :new, :create ]
+  resources :restaurants, except: [:update, :destroy, :edit] do
+    resources :reviews, only: [ :create ]
   end
 end
